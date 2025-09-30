1c83fda7c20151463b4cc5c917b6fdab1837498d
Muxlisa <muxlisa@gmail.com>
2025-09-30 13:28:08 +0500

    not all

---
+++ configapp/models/__init__.py
@@ -0,0 +1,7 @@
+from .auth_user import *
+from .staff_model import *
+from .teach_models import *
+from .group_models import *
+from .attendance import *
+from .about_models import *
+from .news_data import *
